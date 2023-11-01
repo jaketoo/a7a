43435c80111bef18d53d03f57589b5fccfb774eb
jaketoo <140762503+jaketoo@users.noreply.github.com>
2023-11-01 11:45:28 +0200

    Update menfcall.py

--- AnonXMusic/plugins/play/menfcall.py
+++ AnonXMusic/plugins/play/menfcall.py
@@ -11,7 +11,7 @@ from pytgcalls.exceptions import (NoActiveGroupCall,TelegramServerError,AlreadyJ
 async def strcall(client, message):
     assistant = await group_assistant(Anony,message.chat.id)
     try:
-        await assistant.join_group_call(message.chat.id, AudioPiped("./assets/vega.mp3"), stream_type=StreamType().pulse_stream)
+        
         text="😎🥰 المجانين  المتواجدين في الكول :\n\n"
         participants = await assistant.get_participants(message.chat.id)
         k =0
