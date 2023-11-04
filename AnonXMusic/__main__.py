9378609d4f1922c6d9d5fa2b434d7683ca94d243
jaketoo <140762503+jaketoo@users.noreply.github.com>
2023-11-04 16:55:21 +0200

    Update __main__.py

--- AnonXMusic/__main__.py
+++ AnonXMusic/__main__.py
@@ -11,7 +11,6 @@ from AnonXMusic.misc import sudo
 from AnonXMusic.plugins import ALL_MODULES
 from AnonXMusic.utils.database import get_banned_users, get_gbanned
 from config import BANNED_USERS
-from AnonXMusic.plugins.play import azan
 
 async def init():
     if (
@@ -56,7 +55,6 @@ async def init():
     await app.stop()
     await userbot.stop()
     LOGGER("AnonXMusic").info("Stopping AnonX Music Bot...")
-    await azan()
 
 if __name__ == "__main__":
     asyncio.get_event_loop().run_until_complete(init())
