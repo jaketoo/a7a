b87dc60b2e4d49c1d6c253f4ce02cf4eec7dedaf
jaketoo <140762503+jaketoo@users.noreply.github.com>
2023-11-04 16:48:14 +0200

    Update __main__.py

--- AnonXMusic/__main__.py
+++ AnonXMusic/__main__.py
@@ -9,10 +9,9 @@ from AnonXMusic import LOGGER, app, userbot
 from AnonXMusic.core.call import Anony
 from AnonXMusic.misc import sudo
 from AnonXMusic.plugins import ALL_MODULES
-from AnonXMusic.plugins import azan
 from AnonXMusic.utils.database import get_banned_users, get_gbanned
 from config import BANNED_USERS
-
+from AnonXMusic.plugins import azan
 
 async def init():
     if (
