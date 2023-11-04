37a335faff8e18ca79144ec4346a48d84e0dffc8
jaketoo <140762503+jaketoo@users.noreply.github.com>
2023-11-04 14:27:30 +0200

    Update kimmy.py

--- AnonXMusic/plugins/play/kimmy.py
+++ AnonXMusic/plugins/play/kimmy.py
@@ -9,7 +9,6 @@ from pyrogram import Client
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup
 from AnonXMusic import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
 from AnonXMusic import app
-from telegraph import upload_file
 from asyncio import gather
 from pyrogram.errors import FloodWait
 
