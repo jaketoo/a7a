542b4e9c84501f05189c6eff9a7513ad3887e512
jaketoo <140762503+jaketoo@users.noreply.github.com>
2023-11-22 16:24:50 +0200

    Update ملصقات.py

--- AnonXMusic/plugins/play/ملصقات.py
+++ AnonXMusic/plugins/play/ملصقات.py
@@ -30,7 +30,7 @@ async def block_stickers(client:Client, message:Message):
 )
 async def block_stickers(client:Client, message:Message):
     get = await client.get_chat_member(message.chat.id, message.from_user.id)
-    if get.status in ["creator", ChatMemberStatus.ADMINISTRATOR]:
+    if get.status in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR]:
         if message.chat.id in stiklok:
             return await message.reply_text(f"يا {message.from_user.mention} الملصقات مقفله من قبل")
         stiklok.append(message.chat.id)
