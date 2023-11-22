4c1cd1720bb1cb9c87fc57ff4620bf86d2fe1f64
jaketoo <140762503+jaketoo@users.noreply.github.com>
2023-11-22 12:30:26 +0200

    Update call.py

--- AnonXMusic/plugins/play/call.py
+++ AnonXMusic/plugins/play/call.py
@@ -3,9 +3,9 @@ from pyrogram import Client, filters
 from strings import get_command
 from strings.filters import command
 from pyrogram.types import Message, VoiceChatStarted, VoiceChatEnded
-from AnonX import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
-from AnonX.core.call import Anon
-from AnonX.utils.database import get_assistant
+from AnonXmusic import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
+from AnonXmusic.core.call import Anon
+from AnonXmusic.utils.database import get_assistant
 
 @app.on_message(filters.voice_chat_started)
 async def stcall(client: Client, message: Message): 
