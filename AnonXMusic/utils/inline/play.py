ae5879b05f906fbb597d54a2a5a64f11c7b88bfc
jaketoo <140762503+jaketoo@users.noreply.github.com>
2023-11-22 18:43:59 +0200

    Update play.py

--- AnonXMusic/utils/inline/play.py
+++ AnonXMusic/utils/inline/play.py
@@ -66,7 +66,7 @@ def stream_markup_timer(_, chat_id, played, dur):
                 callback_data="GetTimer",
             )
         ],
-        InlineKeyboardButton(text=f"{OWNER_NAME}", url="https://t.me/Mvhmed"),
+    [InlineKeyboardButton(text=f"{OWNER_NAME}", url="https://t.me/Mvhmed")],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="ᴄʟᴏꜱᴇ")],
     ]
     return buttons
@@ -81,7 +81,7 @@ def stream_markup(_, chat_id):
             InlineKeyboardButton(text="ꜱᴋɪᴘ", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="ꜱᴛᴏᴘ", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
-        InlineKeyboardButton(text=f"{OWNER_NAME}", url="https://t.me/Mvhmed"),
+        [InlineKeyboardButton(text=f"{OWNER_NAME}", url="https://t.me/Mvhmed")],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="ᴄʟᴏꜱᴇ")],
     ]
     return buttons
