f12d945508717d7be076f7848bbc5b1559fe2c6d
jaketoo <140762503+jaketoo@users.noreply.github.com>
2023-11-24 13:56:24 +0200

    Update تاك.py

--- AnonXMusic/plugins/play/تاك.py
+++ AnonXMusic/plugins/play/تاك.py
@@ -77,7 +77,7 @@ async def nummmm(client: app, message):
       if not x.user.is_deleted:
        i += 1
        txt += f" {x.user.mention} ›"
-       if i == 20:
+       if i == 1:
         try:
               if not message.photo:
                     await client.send_message(message.chat.id, f"{zz}\n{txt}")
