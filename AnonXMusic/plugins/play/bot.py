b960e0cfd01ad02dc2e69a0d895f426add69fc5c
jaketoo <140762503+jaketoo@users.noreply.github.com>
2023-12-05 21:00:50 +0200

    Update bot.py

--- AnonXMusic/plugins/play/bot.py
+++ AnonXMusic/plugins/play/bot.py
@@ -29,5 +29,5 @@ async def Italymusic(client: Client, message: Message):
     except Exception as e:
         print(e)
         rank = "مش عرفنلو مله ده"
-    async for photo in client.get_chat_photo("me", limit=1):
+    async for photo in client.get_chat_photos("me", limit=1):
                     await message.reply_photo(photo.file_id,       caption=f"""**Hi {star}. \n\nMy Name iS {bot_name}. **\n\n**A Strong Telegram Bot To Play Music & Video iN The Voice Chat.**\n\n**Just Add Me To Your Group And Send** /help .""", reply_markup=keyboard)
